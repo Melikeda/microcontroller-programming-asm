1ec6bc7d558abac4a36748c7302989f6c7ca8f57
Melike Külahcı <m.edakulahci@gmail.com>
2026-03-27 14:40:05 +0300

    Update 5.asm

--- 5.asm
+++ 5.asm
@@ -1,9 +1,10 @@
-;===PROG5.ASM=====================================
+;===PROG5.ASM=====================================  ; ile başlayan satırlar yorum satırıdır.
 LIST P=16F628A
 INCLUDE "P16F628A.INC"
 __CONFIG _INTRC_OSC_NOCLKOUT & _WDT_OFF & _PWRTE_ON & _MCLRE_ON & _BODEN_OFF& _LVP_OFF & _CP_OFF
+
 CLRF PORTB			;portb is cleared.
-COUNTER1 EQU h'20'
+COUNTER1 EQU h'20' ;EQU bir sabite isim vermek için kullanılır
 BANKSEL TRISB		;go to bank1
 MOVLW H'FE'			;h'FE'->W
 MOVWF TRISB			;W->trisb ---  all the bits except 1 of portb are output.
