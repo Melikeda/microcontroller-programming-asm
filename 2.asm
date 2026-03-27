9113ffce4ed920fee950595568f9683e61bd6995
Melike Külahcı <m.edakulahci@gmail.com>
2026-03-27 14:41:40 +0300

    Update 2.asm

--- 2.asm
+++ 2.asm
@@ -1,16 +1,26 @@
 ;===PROG2.ASM=====================================
+
 LIST P=16F628A
 INCLUDE "P16F628A.INC"
-__CONFIG _INTRC_OSC_NOCLKOUT & _WDT_OFF & _PWRTE_ON & _MCLRE_ON & _BODEN_OFF& _LVP_OFF & _CP_OFF
-ORG h'0000'
-CLRF PORTB
-BANKSEL TRISB
-CLRF TRISB
-BANKSEL PORTB
-STARTLABEL
-MOVLW h'55'
-MOVWF PORTB
-LOOPLABEL
-GOTO LOOPLABEL
-END
-;---------------------------
+
+; Mikrodenetleyici konfigürasyon ayarları yapılır
+__CONFIG _INTRC_OSC_NOCLKOUT & _WDT_OFF & _PWRTE_ON & _MCLRE_ON & _BODEN_OFF & _LVP_OFF & _CP_OFF
+
+ORG h'0000'          ; Program 0x0000 adresinden başlar
+
+CLRF PORTB           ; PORTB tüm bitleri 0 yapılır
+
+BANKSEL TRISB        ; TRISB register bankına geçilir
+CLRF TRISB           ; PORTB pinlerinin hepsi çıkış yapılır (0 = output)
+
+BANKSEL PORTB        ; PORTB register bankına geri dönülür
+
+STARTLABEL           ; Ana program başlangıcı
+
+MOVLW h'55'          ; W register’a 0x55 (01010101) yüklenir
+MOVWF PORTB          ; Bu değer PORTB’ye yazılır
+
+LOOPLABEL            ; Sonsuz döngü etiketi
+GOTO LOOPLABEL       ; Program burada sonsuza kadar kalır
+
+END                  ; Program sonu
